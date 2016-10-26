f5ad8a69ac855f2f298fa72dc387fab69fcfd7e0
Tom Dickman <tdickman@gmail.com>
2016-10-25 21:10:23 -0500

    Fixed login

--- goesnotify/run.py
+++ goesnotify/run.py
@@ -1,6 +1,6 @@
 from pushbullet import Pushbullet
 import requests
-from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
 import os
 
 
@@ -14,14 +14,15 @@ class GOES(object):
         '''Find new offers, and compare'''
         s = requests.Session()
         r = s.get('https://goes-app.cbp.dhs.gov/')
-        r = s.post('https://goes-app.cbp.dhs.gov/pkmslogin.form', data={
-            'username': username,
-            'password': password,
+        r = s.post('https://goes-app.cbp.dhs.gov/goes/jsp/j_security_check', data={
+            'j_username': username,
+            'j_password': password,
             'login-form-type': 'pwd',
             'Sign In': 'Sign In'
         })
         r = s.get('https://goes-app.cbp.dhs.gov/main/goes/HomePagePreAction.do')
-        soup = BeautifulSoup(r.text)
+        print(r.text)
+        soup = BeautifulSoup(r.text, 'html.parser')
         token = soup.find('input', {'name': 'org.apache.struts.taglib.html.TOKEN'})['value']
         r = s.post('https://goes-app.cbp.dhs.gov/main/goes/HomePagePostAction.do', data={
             'org.apache.struts.taglib.html.TOKEN': token,
@@ -30,7 +31,7 @@ class GOES(object):
             'pageIndex': '',
             'homepageProgramIndex': 0
         })
-        soup = BeautifulSoup(r.text)
+        soup = BeautifulSoup(r.text, 'html.parser')
         token = soup.find('input', {'name': 'org.apache.struts.taglib.html.TOKEN'})['value']
         r = s.post('https://goes-app.cbp.dhs.gov/main/goes/SelectEnrollmentCenterPostAction.do', data={
             'org.apache.struts.taglib.html.TOKEN': token,
@@ -38,7 +39,7 @@ class GOES(object):
             'page': 5,
             'selectedEnrollmentCenter': 7820
         })
-        soup = BeautifulSoup(r.text)
+        soup = BeautifulSoup(r.text, 'html.parser')
         item = soup.find('div', {'class': 'maincontainer'})
         if item and ('' in item.text):
             return False
@@ -49,13 +50,13 @@ class GOES(object):
     def send_notification(self, link):
         '''Sends a notification for the given item, and then marks it as sent in firebase'''
         self.pb_chan.push_link('GOES Availability Opened Up', 'https://goes-app.cbp.dhs.gov')
-        print 'Sent notification for {}'.format(link)
+        print('Sent notification for {}'.format(link))
 
 
 env = os.environ
 goes = GOES(env['PB_KEY'], env['PB_CHAN'])
 if goes.check_availability(env['USERNAME'], env['PASSWORD']):
-    print "Available!"
+    print("Available!")
     goes.send_notification('https://goes-app.cbp.dhs.gov/')
 else:
-    print "Not Available!"
+    print("Not Available!")
